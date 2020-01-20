077adcfea9c373a932b16c946ff48641889325ea
debauchery1st <trevor.j.martin@gmail.com>
2020-01-20 16:25:13 -0500

    consume context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import data from "./data";
 
+// Context
+import ProductContext from "./contexts/ProductContext";
+
 // Components
-import Navigation from "./components/Navigation";
 import Products from "./components/Products";
+import Navigation from "./components/Navigation";
 import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
@@ -19,15 +22,12 @@ function App() {
   return (
     <div className="App">
       <Navigation cart={cart} />
-
-      {/* Routes */}
-      <Route
-        exact
-        path="/"
-        render={() => <Products products={products} addItem={addItem} />}
-      />
-
-      <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      <ProductContext.Provider value={[products, addItem]}>
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        </Switch>
+      </ProductContext.Provider>
     </div>
   );
 }
